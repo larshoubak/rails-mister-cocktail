a94dfa40edbb415d576b3f8896eb0e267d0ff157
Lars Houbak <lars.houbak@gmail.com>
2019-11-08 15:03:00 +0100

    update with new controller setup

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,8 +5,8 @@ class CocktailsController < ApplicationController
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
@@ -25,7 +25,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'cocktails#index'
   resources :cocktails do
-  resources :doses, only: [ :new, :create]
+  resources :doses, only: [:create]
   end
 
   resources :doses, only: [:destroy]
